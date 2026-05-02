1a55ce22f3097cef28568395e081a3facfdb311c
grant <gthiele2028@berkeley.edu>
2026-05-01 17:45:41 -0700

    another exercise completed

---
+++ C# Fundamentals/DecisionLogicExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+// Your code goes here
+if (daysUntilExpiration > 10){
+    
+}
+else if (daysUntilExpiration > 5) 
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+} 
+else if (daysUntilExpiration > 1) 
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.\nRenew now and save 10%!");
+} 
+else if (daysUntilExpiration == 1) 
+{
+    Console.WriteLine("Your subscription expires within a day!\nRenew now and save 20%!");
+}
+else
+{
+    Console.WriteLine("Your subscription has expired.");
+}
