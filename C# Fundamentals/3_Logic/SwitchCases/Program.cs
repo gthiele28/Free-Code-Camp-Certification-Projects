7359d6385606a5bc2095bc78ce3fa76ebf1d1c37
grant <gthiele2028@berkeley.edu>
2026-05-17 14:47:39 -0700

    part 3 of section 3 done

---
+++ C# Fundamentals/3_Logic/SwitchCases/Program.cs
@@ -0,0 +1,90 @@
+﻿int employeeLevel = 100;
+string employeeName = "John Smith";
+
+string title = "";
+
+switch (employeeLevel)
+{
+    case 100:
+        //title = "Junior Associate";
+        //break;
+    case 200:
+        title = "Senior Associate";
+        break;
+    case 300:
+        title = "Manager";
+        break;
+    case 400:
+        title = "Senior Manager";
+        break;
+    default:
+        title = "Associate";
+        break;
+}
+
+Console.WriteLine($"{employeeName}, {title}");
+
+//Challenge: rewrite an if-else as a switch
+
+// SKU = Stock Keeping Unit. 
+// SKU value format: <product #>-<2-letter color code>-<size code>
+string sku = "01-MN-L";
+
+string[] product = sku.Split('-');
+
+string type = "";
+string color = "";
+string size = "";
+
+switch (product[0])
+{
+    case "01":
+        type = "Sweat shirt";
+        break;
+    case "02":
+        type = "T-Shirt";
+        break;
+    case "03":
+        type = "Sweat pants";
+        break;
+    default:
+        type = "Other";
+        break;
+}
+
+//Console.WriteLine(type);
+
+switch (product[1]) 
+{
+    case "BL":
+        color = "Black";
+        break;
+    case "MN":
+        color = "Maroon";
+        break;
+    default:
+        color = "White";
+        break;
+}
+
+//Console.WriteLine(color);
+
+switch (product[2]) 
+{
+    case "S":
+        size = "Small";
+        break;
+    case "M":
+        size = "Medium";
+        break;
+    case "L":
+        size = "Large";
+        break;
+    default:
+        size = "One size fits all";
+        break;
+}
+
+//Console.WriteLine(size);
+
+Console.WriteLine($"Product: {size} {color} {type}");
