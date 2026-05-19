7edc8b82ca1a9b4a023dceab1183e6ef1f6b6662
grant <gthiele2028@berkeley.edu>
2026-05-19 11:43:28 -0700

    section 4 part 1 done

---
+++ C# Fundamentals/4_VariableData/DataTypes/Program.cs
@@ -0,0 +1,53 @@
+﻿Console.WriteLine("Signed integral types:");
+
+Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
+Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
+Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
+Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
+
+Console.WriteLine("");
+Console.WriteLine("Unsigned integral types:");
+
+Console.WriteLine($"byte   : {byte.MinValue} to {byte.MaxValue}");
+Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
+Console.WriteLine($"uint   : {uint.MinValue} to {uint.MaxValue}");
+Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
+
+Console.WriteLine("");
+Console.WriteLine("Floating point types:");
+Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
+Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
+Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
+
+//int[] data;
+//data = new int[3];
+int[] data = new int[3]; //Same as above 2 commented lines
+
+
+//Even though the string is also an array, it's used so often that it's been given special support
+//This means it can be defined without the new keyword
+//It's a 'reference type' that acts like a normal variable
+//Note that it's still treated with the new keyword, etc. behind the scenes
+string shortenedString = "Hello World!";
+Console.WriteLine(shortenedString);
+
+int val_A = 2;
+int val_B = val_A;
+val_B = 5;
+
+Console.WriteLine("--Value Types--");
+Console.WriteLine($"val_A: {val_A}");
+Console.WriteLine($"val_B: {val_B}");
+
+int[] ref_A= new int[1];
+ref_A[0] = 2;
+int[] ref_B = ref_A; //both referencing same memory location
+ref_B[0] = 5; //when this change occurs, the point in memory both variables use is updated
+
+//both are now 5
+Console.WriteLine("--Reference Types--");
+Console.WriteLine($"ref_A[0]: {ref_A[0]}");
+Console.WriteLine($"ref_B[0]: {ref_B[0]}");
+
+//If datatypes already exist, don't reinvent the wheel
+//Google what datatypes could already exist to solve a problem before making one to save on bloat
