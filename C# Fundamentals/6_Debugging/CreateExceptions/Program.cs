bcec95b306c02a4b764de4549dd2c02e91de21a7
grant <gthiele2028@berkeley.edu>
2026-05-22 12:24:28 -0700

    section 6 part 4

---
+++ C# Fundamentals/6_Debugging/CreateExceptions/Program.cs
@@ -0,0 +1,16 @@
+﻿//Creating an Exception Object
+ArgumentException invalidArgumentException = new ArgumentException("ArgumentException: The 'GraphData' method received data outside the expected range.");
+throw invalidArgumentException;
+
+//Can also create the object within the throw statement
+throw new FormatException("FormatException: Calculations in process XYZ have been cancelled due to invalid data format.");
+
+//Or re-throw an exception from inside a catch block
+catch (Exception ex)
+{
+    // handle or partially handle the exception
+    // ...
+
+    // re-throw the original exception object for further handling down the call stack
+    throw;
+}
